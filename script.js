4cb4dd5a6cd96f6ecd781c6b12078aedd05c3678
Dennis Ammen <dennis.ammen@outlook.com>
2022-05-23 10:46:49 +0200

    implemented infinite scroll

--- script.js
+++ script.js
@@ -1,26 +1,55 @@
 const imageContainer = document.getElementById('image-container');
 const loader = document.getElementById('loader');
 
-let photosArray = [];
-
-const count = 10;
+const count = 30;
 const apiKey = 'kInet-8G-0HtHy0xDHpL0YgYj-7cVkYLeGuNSzGRCIA';
 const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
 
+let photosArray = [];
+let ready = false;
+let imagesLoaded = 0;
+let totalImages = 0;
+
+
+function imageLoaded() {
+    imagesLoaded++;
+    if(imagesLoaded === totalImages) {
+        ready = true;
+        console.log("ready =", ready);
+    }
+}
+
+function setAttributes(element, attributes) {
+    for(const key in attributes) {
+        element.setAttribute(key, attributes[key]);
+    }
+}
+
 function displayPhotos() {
+    imagesLoaded = 0;
+    totalImages = photosArray.length;
+    console.log("total images = ", totalImages);
     photosArray.forEach((photo) => {
         const item = document.createElement('a');
-        item.setAttribute('href', photo.links.html);
-        item.setAttribute('target', '_blank');
         const img = document.createElement('img');
-        img.setAttribute('src', photo.urls.regular);
-
+        
         let description = photo.description;
         if(!description) {
             description = "No description available."
         }
-        img.setAttribute('alt', description);
-        img.setAttribute('title', description);
+        
+        setAttributes(item, {
+            href: photo.links.html,
+            target: 'blank',
+        });
+
+        setAttributes(img, {
+            src: photo.urls.regular,
+            alt: description,
+            title: description,
+        });
+
+        img.addEventListener('load', imageLoaded);
 
         item.appendChild(img);
         imageContainer.appendChild(item);
@@ -33,8 +62,16 @@ async function fetchPhotos() {
         photosArray = await response.json();
         displayPhotos();
     } catch (error) {
+        alert("The following error occured: " + error);
         console.log(error);
     }
 }
+
+window.addEventListener('scroll', () => {
+    if(window.innerHeight + window.scrollY >= document.body.offsetHeight - 1000 && ready) {
+        ready = false;
+        fetchPhotos();
+    }
+})
 
 fetchPhotos();
